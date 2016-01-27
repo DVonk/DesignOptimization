b062017c3c0c023f28af2bb3085f8be33711f67a
SeppelSosna <seppel17@gmx.de>
2016-01-27 14:13:40 +0100

    no message

--- BlueJ Project/HangingLetters.java
+++ BlueJ Project/HangingLetters.java
@@ -15,25 +15,28 @@ import inf.minife.view2.Viewer2;
 public class HangingLetters {
 
     private Model model;
-    //H E L O W R D
+    // H E L O W R D   Bei gelegenheit vllt mal alphabetisch sortieren
     private int[][][] letternodes = {{{0,0},{0,10},{0,20},{10,0},{10,10},{10,20}}, {{0,0},{0,10},{0,20},{10,20}}, {{0,0},{0,10},{0,20},{10,0},{10,10},{10,20}}, 
                                     {{0,0},{0,10},{0,20},{10,0},{10,10},{10,20}}, {{0,0},{2,20},{5,10},{8,20},{10,0}}, {{0,0},{0,10},{0,20},{10,0},{10,10},{10,20}},
                                     {{0,0},{0,10},{0,20},{10,10}}};
-    private int[][][][] letterconnects = {{ {{0,0},{0,2}}, {{1,0},{1,2}}, {{0,1},{1,1}}} // H    
+    
+    private int[][][] letterElements = { {{1,3}, {3,5}, {3,4}, {2,4},{4,6}}, // H
+                                        {},  // E
                                         };
+                                    
 
     public static void main(String[] args) {
         Model m = new HangingLetters().getModel();
 
         m.printStructure();
-        m.solve();
+        //m.solve();
         m.printResults();
 
         Viewer2 viewer = new Viewer2(m);
         viewer.setVisible(true);
     }
 
-    public int[][] letterNodes(char c)
+    public int[][] getLetterNodes(char c)
     {
        switch(c)
        {
@@ -48,8 +51,23 @@ public class HangingLetters {
         }
     }
     
+    public int[][] getLetterElements(char c)
+    {
+       switch(c)
+       {
+           case 'H': return letterElements[0];
+           case 'E': return letterElements[1];
+           case 'L': return letterElements[2];
+           case 'O': return letterElements[3];
+           case 'W': return letterElements[4];
+           case 'R': return letterElements[5];
+           case 'D': return letterElements[6];
+           default: return letterElements[0];
+        }
+    }
+    
     public HangingLetters() {
-
+        String line1 = "H";
         // model
         model = new Model();
 
@@ -57,7 +75,27 @@ public class HangingLetters {
         double rho = 7.88E-6; // kg/mm^2 (density of steel)
         model.createMaterial(1, E, rho);
 
-        for (int i = 1; i <= 3; i++) {
+        //Buchstaben in String duchgehen
+        int id = 0;
+        for(int i=0; i<line1.length(); i++)
+        {
+            //Nodes in Buchstaben durchgehen
+            int[][] nodes = getLetterNodes(line1.charAt(i));
+            for(int j=0; j<nodes.length; j++)
+            {
+                int[] thisNode = nodes[j];
+                // Ich glaub das rechnet in mm, darum alles x 100
+                 //createNode(int id, double x, double y, double z)
+                 System.out.println(thisNode[0] + ":" + thisNode[1]);
+                model.createNode(id, thisNode[0]*100, thisNode[1]*100, 0);
+                id++;
+            }
+        }
+        
+        
+        
+        
+        for (int i = 1; i <= id; i++) {
             RectangleS r;
             r = model.createSection(i, RectangleS.TYPE, Truss2D.TYPE);
             r.setTKY(10); // mm
@@ -68,18 +106,18 @@ public class HangingLetters {
         }
 
         // nodes
-        double b = 1000.0; // mm
-        double h = 1000.0; // mm
-        model.createNode(1, -b, 0, 0);
-        model.createNode(2, 0, 0, 0);
-        model.createNode(3, b, 0, 0);
-        model.createNode(4, 0, -h, 0);
+        //double b = 1000.0; // mm
+        //double h = 1000.0; // mm
+        //model.createNode(100, -b, 0, 0);
+        //model.createNode(200, 0, 0, 0);
+        //model.createNode(300, b, 0, 0);
+        //model.createNode(400, 0, -h, 0);
 
         // forces
         Force f = new Force();
         f.setValue(DOF.T_X, 30000.0); // N
         f.setValue(DOF.T_Y, -20000.0); // N
-        model.getNode(4).setForce(f);
+        model.getNode(id-1).setForce(f);
 
         // constraints
         Constraint c = new Constraint();
@@ -87,15 +125,31 @@ public class HangingLetters {
         c.setFree(DOF.T_Y, false);
         model.getNode(1).setConstraint(c);
         model.getNode(2).setConstraint(c);
-        model.getNode(3).setConstraint(c);
-
+        
+        System.out.println("REALTABLES:");
+        System.out.println(model.getRealtables().length);
+        
+        int elemID = 1;
         // elements
-        model.createElement(1, Truss2D.TYPE, model.getMaterial(1),
-                model.getRealtable(1), model.getNode(1), model.getNode(4));
-        model.createElement(2, Truss2D.TYPE, model.getMaterial(1),
-                model.getRealtable(2), model.getNode(2), model.getNode(4));
-        model.createElement(3, Truss2D.TYPE, model.getMaterial(1),
-                model.getRealtable(3), model.getNode(3), model.getNode(4));
+        for(int i=0; i<line1.length(); i++)
+        {
+            int [][] elems  = getLetterElements(line1.charAt(i));
+            for(int j=0; j<elems.length-1;j++)
+            {
+                System.out.println("ELEMID:");
+                System.out.println(elemID);
+                int startNode = elems[j][0];
+                int endNode = elems[j][1];
+                System.out.println(startNode + ":" + endNode);
+                model.createElement(elemID, Truss2D.TYPE, model.getMaterial(1), model.getRealtable(elemID), model.getNode(startNode), model.getNode(endNode));
+                elemID++;
+            }
+        }
+        
+        //model.createElement(1, Truss2D.TYPE, model.getMaterial(1),
+        //        model.getRealtable(1), model.getNode(1), model.getNode(4));
+        //model.createElement(2, Truss2D.TYPE, model.getMaterial(1),
+        //        model.getRealtable(2), model.getNode(2), model.getNode(4));
     }
 
     public Model getModel() {
